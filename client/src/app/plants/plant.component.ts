11c37a77ccac133586a5f8e1838c3fab30e5f06a
Laverne Schrock <schr1230@morris.umn.edu>
2017-03-04 15:13:57 -0600

    Implement ability to view arbitrary plants from client

--- client/src/app/plants/plant.component.ts
+++ client/src/app/plants/plant.component.ts
@@ -1,31 +1,30 @@
 
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
 import {Plant} from './plant';
 import {PlantService} from './plant.service';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
     selector: 'plant-component',
     template: `
         <p> Hello World!</p>
         <p> {{plant.commonName}}</p>
+        <p> {{plant.cultivar}}</p>
         `
 })
 
 export class PlantComponent implements OnInit {
     private plant: Plant = {id: "", commonName: "", cultivar: "", gardenLocation: ""};
-    constructor(private plantService: PlantService){
-    }
+    constructor(private plantService: PlantService,
+                private route: ActivatedRoute,
+    ){ }
 
 
     ngOnInit() :void{
-        this.plantService.getPlantById('58b8f2565fbad0fc7a89f747').subscribe(
-            plant => {
-                this.plant = plant;
-            },
-            err => {
-                console.log(err);
-            }
-        );
+        this.route.params
+            .switchMap((params:Params) => this.plantService.getPlantById(params['id']))
+            .subscribe(plant => this.plant = plant);
     }
 
 }
