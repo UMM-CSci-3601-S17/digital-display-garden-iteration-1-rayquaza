620fd54e8f3b7c1fff9877168e74feb887c5a5e8
Laverne Schrock <schr1230@morris.umn.edu>
2017-03-09 12:05:55 -0600

    Load rows into Documents

--- server/src/main/java/umm3601/parsing/ParseExcel.java
+++ server/src/main/java/umm3601/parsing/ParseExcel.java
@@ -5,14 +5,14 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.bson.Document;
+
 import static org.apache.poi.ss.usermodel.Cell.CELL_TYPE_STRING;
 import static org.apache.poi.ss.usermodel.Cell.CELL_TYPE_NUMERIC;
 
 
 import java.io.FileInputStream;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Iterator;
+import java.util.*;
 
 
 public class ParseExcel{
@@ -29,6 +29,8 @@ public class ParseExcel{
 
             Sheet sheet = workbook.getSheetAt(sheetNumber);
 
+            List<Document> documentList = new LinkedList<>();
+
             boolean foundEmptyNumberCell = false;
 
             for (Iterator<Row> rowsIT = sheet.rowIterator(); rowsIT.hasNext() && !foundEmptyNumberCell; ) {
@@ -37,21 +39,56 @@ public class ParseExcel{
                 Iterator<Cell> cellsIT = row.cellIterator();
 
                 if ( row.getRowNum() > numHeaderRows - 1) {
+                    Document plant = new Document();
 
                     for (int i = 0; i < 7; i++) {
+
                         Cell cell = cellsIT.next();
+                        switch (i) {
+                            case 0:
+                                // The # column
+                                plant.append("plantId", (int)cell.getNumericCellValue());
+                                break;
+                            case 1:
+                                // Common Name
+                                plant.append("commonName", cell.getStringCellValue());
+                                break;
+                            case 2:
+                                // Cultivar
+                                plant.append("cultivar", cell.getStringCellValue());
+                                break;
+                            case 3:
+                                // Source
+                                plant.append("source", cell.getStringCellValue());
+                                break;
+                            case 4:
+                                // ???
+                                 break;
+                            case 5:
+                                // ????
+                                break;
+                            case 6:
+                                // Garden Location
+                                if (cell.getCellType() == CELL_TYPE_NUMERIC ) {
+                                    plant.append("gardenLocation", new Double(cell.getNumericCellValue()).toString());
+                                } else {
+                                    plant.append("gardenLocation", cell.getStringCellValue());
+                                }
+                                break;
+                        }
+
                         if (cell.getColumnIndex() == 0 && cell.getNumericCellValue() == 0) {
                             foundEmptyNumberCell = true;
                             break;
                         }
-                        else if (cell.getCellType() == CELL_TYPE_NUMERIC ) {
-                            System.out.print(cell.getNumericCellValue() + ", ");
-                        } else {
-                            System.out.print(cell.getStringCellValue()+ ", ");
-                        }
+//                        else if (cell.getCellType() == CELL_TYPE_NUMERIC ) {
+//                            System.out.print(cell.getNumericCellValue() + ", ");
+//                        } else {
+//                            System.out.print(cell.getStringCellValue()+ ", ");
+//                        }
 
                     }
-                    System.out.println();
+                    System.out.println(plant);
                 }
 
             }
